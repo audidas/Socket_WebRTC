d73054203bdd6acf35d27a4f1a28f22fc0fb8b98
audidas <audidas@telepix.net>
2023-01-25 15:00:49 +0900

    WebRTC 화상영상 구현

--- src/server.js
+++ src/server.js
@@ -1,5 +1,8 @@
 import http from "http";
+import https from "https";
+import fs from "fs";
 import express from "express";
+import SocketIO from "socket.io";
 
 const app = express();
 
@@ -13,4 +16,31 @@ app.get("/*", (req, res) => res.redirect("/"));
 const handleListen = () => console.log(`Listening on http://localhost:3000`);
 
 const httpServer = http.createServer(app);
+const httpsServer = https.createServer(
+  {
+    key: fs.readFileSync(__dirname + "/key.pem", "utf-8"),
+    cert: fs.readFileSync(__dirname + "/cert.pem", "utf-8"),
+  },
+  app.use("/", (req, res) => {
+    res.render("home");
+  })
+);
+
+const wsServer = SocketIO(httpServer);
+
+wsServer.on("connection", (socket) => {
+  socket.on("join_room", (roomName) => {
+    socket.join(roomName);
+    socket.to(roomName).emit("welcome");
+  });
+  socket.on("offer", (offer, roomName) => {
+    socket.to(roomName).emit("offer", offer);
+  });
+  socket.on("answer", (answer, roomName) => {
+    socket.to(roomName).emit("answer", answer);
+  });
+  socket.on("ice", (ice, roomName) => {
+    socket.to(roomName).emit("ice", ice);
+  });
+});
 httpServer.listen(3000, handleListen);
